fc9b75992b222a8d71cd6a340b72a55d1a04ccd9
mrMuhammadrahim <muhammadrahimzan@gmail.com>
2024-12-03 12:16:30 +0500

    python

---
+++ VazifaPy3.py
@@ -0,0 +1,5 @@
+son1 = int(input("Birinchi sonni kiriting: "))
+son2 = int(input("Ikkinchi sonni kiriting: "))
+son3 = int(input("Uchinchi sonni kiriting: "))
+natija = (son1**2 + son2**2)/son3
+print(f"Natija: {natija}")
